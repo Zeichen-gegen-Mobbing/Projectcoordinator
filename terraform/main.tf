6db6a6793bebef211e724b259a0737b9d7f02709
WhiteTom <whitetom@arcor.de>
2024-11-03 14:13:25 +0100

    feat: add current user as app owner

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,9 @@
-resource "azuread_application_registration" "client" {
+data "azuread_client_config" "current" {
+}
+
+resource "azuread_application" "client" {
   display_name     = "Projectcoordinator-${var.environment}"
   description      = "Application to login to the Projectcoordinator application"
   sign_in_audience = "AzureADMyOrg"
+  owners           = [data.azuread_client_config.current.object_id]
 }
