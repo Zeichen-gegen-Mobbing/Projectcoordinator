95cfc468f5832912240c094ec284cbcd71a8575a
WhiteTom <whitetom@arcor.de>
2025-08-10 21:18:57 +0200

    fix: mark output as sensitive

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,3 +1,5 @@
 output "static_web_app_api_key" {
-  value = azurerm_static_web_app.this.api_key
+  value       = azurerm_static_web_app.this.api_key
+  sensitive   = true
+  description = "The API key for the Azure Static Web App. Used to deploy to in github actions"
 }
