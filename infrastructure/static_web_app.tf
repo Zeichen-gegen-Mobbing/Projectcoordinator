40bd1eedd394010845bf66739e34849dfae1bff6
WhiteTom <whitetom@arcor.de>
2025-08-10 23:26:10 +0200

    fix: germany not valid location

--- infrastructure/static_web_app.tf
+++ infrastructure/static_web_app.tf
@@ -1,5 +1,5 @@
 resource "azurerm_static_web_app" "this" {
   name                = "stapp-ProjectCoordinator-${var.environment}"
   resource_group_name = data.azurerm_resource_group.this.name
-  location            = data.azurerm_resource_group.this.location
+  location            = "westeurope"
 }
