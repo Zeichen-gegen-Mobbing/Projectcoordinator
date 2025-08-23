cceade93f0db99cc7200b09325c0aa1bcbd5bd7c
WhiteTom <whitetom@arcor.de>
2025-08-23 10:58:59 +0200

    fix: use recommended connection string instead legacy

--- infrastructure/static_web_app.tf
+++ infrastructure/static_web_app.tf
@@ -3,11 +3,11 @@ resource "azurerm_static_web_app" "this" {
   resource_group_name = data.azurerm_resource_group.this.name
   location            = "westeurope"
   app_settings = {
-    "Cosmos__ConnectionString"       = data.azurerm_key_vault_secret.cosmos_connection_string.value
-    "Cosmos__DatabaseId"             = var.cosmos_database_name
-    "Cosmos__ContainerId"            = azurerm_cosmosdb_sql_container.places.name
-    "OpenRouteService__ApiKey"       = var.open_route_service_api_key
-    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.this.instrumentation_key
+    "Cosmos__ConnectionString"              = data.azurerm_key_vault_secret.cosmos_connection_string.value
+    "Cosmos__DatabaseId"                    = var.cosmos_database_name
+    "Cosmos__ContainerId"                   = azurerm_cosmosdb_sql_container.places.name
+    "OpenRouteService__ApiKey"              = var.open_route_service_api_key
+    "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.this.connection_string
   }
 
   lifecycle {
