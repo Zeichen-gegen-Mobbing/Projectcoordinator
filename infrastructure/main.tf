a699aa2aee6c22417f326e1707ba8dd08ebdbbfa
WhiteTom <whitetom@arcor.de>
2025-10-19 21:52:32 +0200

    fix: dependency cycle

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,24 +11,21 @@ data "azuread_service_principal" "microsoft_graph" {
   client_id = data.azuread_application_published_app_ids.well_known.result["MicrosoftGraph"]
 }
 
-resource "azuread_application" "client" {
-  display_name     = "Projectcoordinator-${var.environment}"
-  description      = "Application to login to the Projectcoordinator application"
-  sign_in_audience = "AzureADMyOrg"
-  owners           = [data.azuread_client_config.current.object_id]
+resource "azuread_application_registration" "client" {
+  display_name = "Projectcoordinator-${var.environment}"
+  description  = "Application to login to the Projectcoordinator application"
+}
 
-  prevent_duplicate_names = true
+resource "azuread_application_api_access" "graph" {
+  application_id = azuread_application_registration.client.id
+  api_client_id  = data.azuread_service_principal.microsoft_graph.client_id
+  scope_ids      = [data.azuread_service_principal.microsoft_graph.oauth2_permission_scope_ids["User.ReadBasic.All"]]
+}
 
-  required_resource_access {
-    resource_app_id = data.azuread_service_principal.microsoft_graph.client_id
-    resource_access {
-      id   = data.azuread_service_principal.microsoft_graph.oauth2_permission_scope_ids["User.ReadBasic.All"]
-      type = "Scope"
-    }
-  }
-  single_page_application {
-    redirect_uris = concat(["https://${azurerm_static_web_app.this.default_host_name}/authentication/login-callback"], var.redirect_uris)
-  }
+resource "azuread_application_redirect_uris" "static_site" {
+  application_id = azuread_application_registration.client.id
+  type           = "SPA"
+  redirect_uris  = concat(["https://${azurerm_static_web_app.this.default_host_name}/authentication/login-callback"], var.redirect_uris)
 }
 
 resource "azurerm_cosmosdb_sql_container" "places" {
