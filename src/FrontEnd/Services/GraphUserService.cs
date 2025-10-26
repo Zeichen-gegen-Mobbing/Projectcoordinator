fd8e3cf34133a222afa07cc1af7b7dc721c3d070
WhiteTom <whitetom@arcor.de>
2025-10-27 00:17:27 +0100

    fix: match the interface declaration

--- src/FrontEnd/Services/GraphUserService.cs
+++ src/FrontEnd/Services/GraphUserService.cs
@@ -6,18 +6,18 @@ namespace FrontEnd.Services
 {
     public class GraphUserService(IHttpClientFactory httpClientFactory, ILogger<GraphUserService> logger) : IUserService
     {
-        public async Task<User> GetUserAsync(UserId id)
+        public async Task<User> GetUserAsync(UserId Id)
         {
             using (logger.BeginScope(nameof(GetUserAsync)))
             {
                 var httpClient = httpClientFactory.CreateClient("GraphAPI");
 
-                var requestUri = $"users/{id.Value}?$select=displayName,id";
+                var requestUri = $"users/{Id.Value}?$select=displayName,id";
 
-                var graphUser = await httpClient.GetFromJsonAsync<GraphUser>(requestUri) ?? throw new InvalidOperationException($"Failed to retrieve user information for user ID: {id}");
+                var graphUser = await httpClient.GetFromJsonAsync<GraphUser>(requestUri) ?? throw new InvalidOperationException($"Failed to retrieve user information for user ID: {Id}");
                 return graphUser.DisplayName != null
-                    ? new User(id, graphUser.DisplayName)
-                    : new User(id);
+                    ? new User(Id, graphUser.DisplayName)
+                    : new User(Id);
             }
         }
     }
