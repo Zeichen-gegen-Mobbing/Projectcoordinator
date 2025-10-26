5a4840044661f6011eb03cee1fa534c777dfcd02
WhiteTom <whitetom@arcor.de>
2025-10-26 09:09:17 +0100

    docs: reason why we do this

--- src/api/Middleware/AuthorizationHeaderMiddleware.cs
+++ src/api/Middleware/AuthorizationHeaderMiddleware.cs
@@ -5,6 +5,12 @@ using ZgM.ProjectCoordinator.Shared;
 
 namespace api.Middleware;
 
+/// <summary>
+/// Replaces the Authorization header with the value of <see cref="CustomHttpHeaders.SwaAuthorization"/>
+/// As static Web App replaces the Authorization header
+/// we need to use a custom header. To avoid rewriting all code that uses the Authorization header,
+/// we replace it in this middleware.
+/// </summary>
 public class AuthorizationHeaderMiddleware : IFunctionsWorkerMiddleware
 {
     private const string AuthorizationHeader = "Authorization";
