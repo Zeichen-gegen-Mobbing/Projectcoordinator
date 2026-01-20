971fefd799f9e0489571be667047ac46e1aba9c5
WhiteTom <whitetom@arcor.de>
2026-01-20 21:42:26 +0100

    fix: appsettings

--- infrastructure/static_web_app.tf
+++ infrastructure/static_web_app.tf
@@ -7,7 +7,7 @@ resource "azurerm_static_web_app" "this" {
       "Cosmos__ConnectionString"              = data.azurerm_key_vault_secret.cosmos_connection_string.value
       "Cosmos__DatabaseId"                    = var.cosmos_database_name
       "Cosmos__PlacesContainerId"             = azurerm_cosmosdb_sql_container.places.name
-      "Cosmos__PlacesContainerId"             = azurerm_cosmosdb_sql_container.users.name
+      "Cosmos__UsersContainerId"              = azurerm_cosmosdb_sql_container.users.name
       "OpenRouteService__ApiKey"              = var.open_route_service_api_key
       "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.this.connection_string
       "AzureAd__ClientId"                     = module.application_registrations.api_client_id
