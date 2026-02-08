cb27db7bf33c39a7d71812186b12a828bd54b20b
Thimo Limpert <38078578+WhiteTomX@users.noreply.github.com>
2026-02-08 22:01:09 +0100

    fix: add missing scopes (#120)
    Co-authored-by: WhiteTom <whitetom@arcor.de>

--- src/api/GetDefaultSettings.cs
+++ src/api/GetDefaultSettings.cs
@@ -1,14 +1,18 @@
+using api.Extensions;
+using api.Options;
 using api.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace api;
 
 public sealed class GetDefaultSettings(
     IUserSettingsService userSettingsService,
+    IOptions<RoleOptions> roleOptions,
     ILogger<GetDefaultSettings> logger)
 {
     [Authorize(Roles = "projectcoordinator")]
@@ -17,6 +21,10 @@ public sealed class GetDefaultSettings(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "default-settings")] HttpRequest req)
     {
         logger.LogInformation("Getting default cost settings");
+
+        await req.HttpContext.AuthorizeAzureFunctionAsync(
+            scopes: ["Settings.Read"],
+            roles: [roleOptions.Value.ProjectCoordination]);
 
         var settings = await userSettingsService.GetDefaultSettingsAsync();
 
--- src/api/UpsertDefaultSettings.cs
+++ src/api/UpsertDefaultSettings.cs
@@ -1,15 +1,19 @@
+using api.Extensions;
+using api.Options;
 using api.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using ZgM.ProjectCoordinator.Shared;
 
 namespace api;
 
 public sealed class UpsertDefaultSettings(
     IUserSettingsService userSettingsService,
+    IOptions<RoleOptions> roleOptions,
     ILogger<UpsertDefaultSettings> logger)
 {
     [Authorize(Roles = "projectcoordinator")]
@@ -17,6 +21,7 @@ public sealed class UpsertDefaultSettings(
     public async Task<IActionResult> Run(
         [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "default-settings")] HttpRequest req)
     {
+        await req.HttpContext.AuthorizeAzureFunctionAsync(scopes: ["Settings.Write"], roles: [roleOptions.Value.ProjectCoordination]);
         logger.LogInformation("Upserting default cost settings");
 
         var settings = await req.ReadFromJsonAsync<UserSettings>();
